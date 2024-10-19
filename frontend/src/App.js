40819494dc66614aed3f04919b9349ff0c9ad22a
roshanidubey <123582053+roshanidubey@users.noreply.github.com>
2024-10-19 21:37:12 +0530

    Cload integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
+import CloudUpload from './components/cloudupload'; // Use the correct name
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <Router>
-      <Routes> 
+      <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} /> {/* Replace component prop with element */}
-        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/upload" element={<CloudUpload />} /> {/* Add the upload route */}
       </Routes>
     </Router>
   );
